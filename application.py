f41b42dd945d4e9962d20169a56209e6ef96bf2d
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-04-21 23:54:52 -0400

    first commit

---
+++ application.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import os
+import subprocess
+import pendulum
+from flask_socketio import SocketIO
+import csv
+
+# Flask setup
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "DefaultSecret"
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def sleepschedule():
+    return render_template("sleepschedule.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    with open("times.csv", "a") as f:
+        w = csv.writer(f)
+        w.writerow([pendulum.now("America/Toronto").format("YYYY/MM/DD HH:mm:ss")])
+    return "HI"
+
+# Run Flask if file is interpreted
+if __name__ == "__main__":
+    os.environ["FLASK_APP"] = "application.py"
+
+    try:
+        current = subprocess.check_output(["lsof", "-t", "-i:5000"])
+        current = max(current.decode("utf-8").split("\n"))
+        print(f"kill {current}")
+        os.system(f"kill {current}")
+    except:
+        pass
+    os.system("flask run")
