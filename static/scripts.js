96dac93c77cf012e85d226abfd398b847e414cb9
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-04-28 23:31:30 -0400

    added chart.js fancy things

--- static/scripts.js
+++ static/scripts.js
@@ -18,10 +18,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let div = document.createElement("div")
     div.innerHTML = temp[0] + ": "+ temp[4]
-    info_display.append(div)
-    info_display.append(document.createElement("br"))
+    // info_display.append(div)
+    // info_display.append(document.createElement("br"))
   }
 
+  var ctx = document.getElementById('myChart')
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: times.map(x=>moment(x[0], "YYYY/MM/DD").format("ddd MMM D")),
+        datasets: [{
+          label: "Individual Sleep Times",
+          data: times.map(x=>x[3])
+        },{
+          label: "Sleep Times Trend",
+          data: times.map(x=>x[3]),
+          type: "line",
+          borderColor: 'rgba(153, 44, 127)',
+          backgroundColor: 'rgba(153, 44, 127)',
+          pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+          pointBorderColor: 'rgba(0, 0, 0, 0)',
+          fill: false
+        }]
+    },
+    options: {
+      tooltips: {
+        callbacks: {
+          label: function(x) {
+            return moment.duration(parseInt(x["value"]), "minutes").humanize()
+          }
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            // callback: x => {
+            //   return moment(x, "dddd MMM D").format("MMM D")
+            // }
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            callback: x => {
+              return moment.duration(x, "minutes").humanize()
+            }
+          }
+        }]
+      }
+    }
+  })
 
   // Fade out all divs and fade in specific div
   function change_to(div) {
@@ -35,7 +81,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 400)
     return false
   }
-
-  // TODO
-
 })
